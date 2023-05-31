9e27c619bc56fd25bc158c20bdea29726b5c87eb
ramsapkota123 <rbsapkota99@gmail.com>
2023-05-31 18:39:10 -0500

    jdfhs

--- src/start2030/CreateFile.java
+++ src/start2030/CreateFile.java
@@ -4,6 +4,8 @@ public class CreateFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hi");
 
 	}
 
