2df0d1cdfaaa9515c07f6f3309ce3c73183c7cb7
ramsapkota123 <rbsapkota99@gmail.com>
2023-05-31 17:52:33 -0500

    commit is here...

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rbsap
+ *
+ */
+module java2030 {
+}
---
+++ src/start2030/CreateFile.java
@@ -0,0 +1,10 @@
+package start2030;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
